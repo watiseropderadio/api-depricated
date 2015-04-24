211fc0dcfe4bd28d708075269a48163bd762731f
Adriaan van Rossum <git@adriaan.io>
2015-04-24 18:46:17 +0200

    Add version to index file

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ exports.list = function(req, res) {
   res.send(JSON.stringify({
     author: 'Adriaan van Rossum',
     email: 'api@watiseropderadio.nl',
-    website: 'http://watiseropderadio.nl'
+    website: 'http://watiseropderadio.nl',
+    version: '0.0.1'
   }));
 };
