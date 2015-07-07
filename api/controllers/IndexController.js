4832c2987e04c6dba16e0d97fae67cffe711f280
Adriaan van Rossum <git@adriaan.io>
2015-07-07 16:23:54 +0200

    Show index JSON for root path

---
+++ api/controllers/IndexController.js
@@ -0,0 +1,19 @@
+/**
+ * IndexController
+ *
+ * @description :: Server-side logic for managing indices
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  get: function(req, res) {
+    return res.json({
+      name: 'API',
+      description: 'API for serving the data for http://watiseropderadio.nl',
+      author: 'Adriaan van Rossum',
+      contact: 'api@watiseropderadio.nl'
+    });
+  }
+
+};
