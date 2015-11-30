5cdf0eb0d4054fd700a179030776cd330889e113
Adriaan van Rossum <git@adriaan.io>
2015-11-30 19:18:36 +0100

    Get node environment from .env file

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ pm2.connect(function() {
     instances: instances,
     max_memory_restart: maxMemory + 'M', // Auto restart if process taking more than XXmo
     env: { // If needed declare some environment variables
-      "NODE_ENV": "production"
+      "NODE_ENV": process.env.NODE_ENV || "production"
     },
   }, function(err) {
     if (err) return console.error('Error while launching applications', err.stack || err);
