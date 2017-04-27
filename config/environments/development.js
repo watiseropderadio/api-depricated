672cffbab584d1d4321d7a333dc4ce0ce4fafb61
Adriaan van Rossum <git@adriaan.io>
2017-04-27 13:10:23 -0700

    Enable cors

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,4 +1,24 @@
 export default {
+  server: {
+    cors: {
+      origin: '*',
+      enabled: true,
+
+      headers: [
+        'Accept',
+        'Content-Type'
+      ],
+
+      methods: [
+        'GET',
+        'POST',
+        'PATCH',
+        'DELETE',
+        'HEAD',
+        'OPTIONS'
+      ]
+    }
+  },
   logging: {
     level: 'DEBUG',
     format: 'text',
